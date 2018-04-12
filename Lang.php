2538a489af9bb21c853181f86ae41be5af09a03b
Bohdan <balovbohdan@gmail.com>
2018-04-12 08:28:35 +0300

    Working at main language class docs.
    TODO: add example.

--- Lang.php
+++ Lang.php
@@ -1,12 +1,31 @@
 <?php
-namespace libs\balov\php\lang;
+namespace libs\php\lang;
 
 /**
- * Language data of the platform.
+ * Works width language data.
+ * Helps to organize web-site multilanguage as simple as possible.
  * Class Lang
- * @package libs\balov\php\lang
+ * @package libs\php\lang
+ * @author Balov Bohdan <balovbohdan@gmail.com>
+ * @version 1.0.0
+ *
+ * @example
  */
 class Lang {
+    /**
+     * Constructor.
+     * @param array $params
+     * @throws LangException
+     */
+    function __construct(array $params = array()) {
+        $defParams = $this->getDefParams();
+        $params = array_merge($defParams, $params);
+
+        $this->lang = $params["lang"] ?: $defParams["lang"];
+        $this->root = $params["root"] ?: $defParams["root"];
+        $this->data = self::loadLangData($this->root, $this->lang);
+    }
+    
     /**
      * Bulgarian.
      */
@@ -63,13 +82,13 @@ class Lang {
     const UZ = "uz";
 
     /**
-     * Code of labguage.
+     * Language code.
      * @var string
      */
     private $lang = "";
 
     /**
-     * Path to the root of language files.
+     * Path to the root folder of language files.
      * @var string
      */
     private $root = "";
@@ -81,51 +100,20 @@ class Lang {
     private $data = array();
 
     /**
-     * Lang constructor.
-     * @param array $params
-     * @throws LangException
-     */
-    function __construct(array $params = array()) {
-        $defParams = $this->getDefParams();
-        $params = array_merge($defParams, $params);
-
-        $this->lang = $params["lang"] ?: $defParams["lang"];
-        $this->root = $params["root"] ?: $defParams["root"];
-        $this->data = self::loadLangData($this->root, $this->lang);
-    }
-
-    /**
      * Loads language data from file.
-     * @param string $root Path to the root of language files.
-     * @param string $lang Code of language.
-     * @return array
+     * If language file with given language code does not exist tries
+     * to load language data from "UK" language file.
+     * @param string $root Path to the root folder of language files.
+     * @param string $lang Language code. See constants.
+     * @return array Language data.
      * @throws LangException
      */
     private static function loadLangData($root, $lang) {
-        if (!$root) throw new LangException("Got incorrect root of lang files.");
+        if (!$root) throw new LangException("Got incorrect path to the root folder of language files.");
         if (!$lang) throw new LangException("Got incorrect code of language.");
-
         $path = $root . $lang . ".json";
-
         if (!file_exists($path)) return self::loadLangData($root, self::UK);
-
         return json_decode(file_get_contents($path), true) ?: array();
-    }
-
-    /**
-     * Returns path to the root of language files for admin side of the platform.
-     * @return string
-     */
-    static function getTechRoot() {
-        return $_SERVER["DOCUMENT_ROOT"] .  DIRECTORY_SEPARATOR . "teach" . DIRECTORY_SEPARATOR . "lang" . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR;
-    }
-
-    /**
-     * Returns path to the root of language files for student side of the platform.
-     * @return string
-     */
-    static function getStudRoot() {
-        return $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "lang" . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR;
     }
 
     /**
@@ -133,25 +121,29 @@ class Lang {
      * @return array
      */
     function getDefParams() {
-        $defParams = array(
-            "lang" => $_SESSION["settings"]["lang"],
+        // TODO: Here you can define some external sources of language code.
+        // For instance you can use $_GET, $_POST, $_SESSION, $_COOKIE sources.
+        // So you can write:
+        //
+        // return array(
+        //     "lang" => $_SESSION["langCode"] ?: self::UK,
+        //     [...rest_of_default_parameters...]
+        // );
+        
+        return array(
+            "lang" => self::UK,
             "root" => DIRECTORY_SEPARATOR . "teach" . DIRECTORY_SEPARATOR . "lang" . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR
         );
-
-        $defParams["lang"] = $defParams["lang"] ?: self::UK;
-
-        return $defParams;
     }
 
     /**
-     * Returns language data using key.
-     * @param string $key Key to aim language data.
-     * @return string Language data.
+     * Returns language data element.
+     * @param string $key Key to aim language data element.
+     * @return string Language data element.
      * @throws LangException
      */
     function get($key) {
-        if (!$key) throw new LangException("Got incorrect key to find language data.");
-
+        if (!$key) throw new LangException("Got incorrect key to find language data element.");
         return $this->data[$key];
     }
 }
